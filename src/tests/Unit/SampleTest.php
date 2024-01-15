8e22ca29b60a010eecfb6082671abde9d6d6807a
ShunyaSakamoto <syunya27212@gmail.com>
2024-01-16 03:11:10 +0900

    単体テストのサンプル実装

---
+++ src/tests/Unit/SampleTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Artisan;
+use Tests\TestCase;
+
+class SampleTest extends TestCase
+{
+    public function setUp() : void
+    {
+        parent::setUp();
+        // Artisan::call('config:clear');
+        Artisan::call('migrate:refresh');
+        
+        // Userモデル10件
+        User::factory(10)
+            ->create();
+    }
+
+    /**
+     * ユーザー一覧取得
+     *
+     * @author s_sakamoto
+     * @category ユーザー
+     * @return void
+     */
+    public function test_user_get_list() : void
+    {
+        ### Arrange
+        // Nothing to do.
+
+        ### Act
+        $users = User::all();
+
+        ### Assert
+        $this->assertNotNull($users);
+        $this->assertNotEmpty($users);
+        $this->assertSame(10, $users->count(), 'User count is not same as expected.');
+    }
+
+    /**
+     * ユーザ詳細取得
+     *
+     * @author s_sakamoto
+     * @category ユーザー
+     * @return void
+     */
+    public function test_user_get_detail() : void
+    {
+        ### Arrange
+        // Nothing to do.
+
+        ### Act
+        $user = User::query()
+            ->where('id', 1)
+            ->get();
+
+        ### Assert
+        $this->assertNotNull($user);
+        $this->assertNotEmpty($user);
+        $this->assertSame(2, $user->count(), 'User count is not same as expected.'); // わざと期待値を間違えている
+    }
+}
